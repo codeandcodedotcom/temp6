71d137e2a607ea0b4c11ed2e496dae3e4c60f81b
codeandcodedotcom <justincaseanother@gmail.com>
2025-12-24 03:14:42 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -1,12 +1,9 @@
-stmt = (
-    select(
-        month_expr,
+norm = func.lower(func.trim(Project.managed_by))
 
-        func.sum(case((Project.managed_by == "Self Managed", 1), else_=0)).label("self_managed"),
-        func.sum(case((Project.managed_by.in_(["Project Lead","IT Project Lead"]), 1), else_=0)).label("team_lead"),
-        func.sum(case((Project.managed_by == "Project Manager", 1), else_=0)).label("project_manager"),
-        func.sum(case((Project.managed_by == "Team of PM professionals", 1), else_=0)).label("team_of_PM_professionals"),
-    )
-    .group_by(month_expr)
-    .order_by(month_expr)
-)
+func.sum(case(norm == "self managed", 1, else_=0)).label("self_managed"),
+
+func.sum(case(norm.in_(["project lead","it project lead"]), 1, else_=0)).label("team_lead"),
+
+func.sum(case(norm == "project manager", 1, else_=0)).label("project_manager"),
+
+func.sum(case(norm == "team of pm professionals", 1, else_=0)).label("team_of_PM_professionals"),
