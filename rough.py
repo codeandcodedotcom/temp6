b23fdb53e83f922b2f215fef975eb6857c9ed1f7
codeandcodedotcom <justincaseanother@gmail.com>
2026-02-07 00:21:31 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -1,5 +1,20 @@
 from pathlib import Path
+from tempfile import gettempdir
 
-safe_tmp = Path(os.getenv("TMPDIR", "/tmp")) / "mcs-apd-lit-tmp"
+TMP_ROOT = Path(gettempdir()) / "mcs-apd-lit-tmp"
+TMP_ROOT.mkdir(parents=True, exist_ok=True)
+
+PDF_DIR = TMP_ROOT / "generated_pdf"
+PDF_DIR.mkdir(parents=True, exist_ok=True)
+
+PDF_URL_PREFIX = "/pdf"
+
+
+
+from pathlib import Path
+from tempfile import gettempdir
+
+safe_tmp = Path(gettempdir()) / "mcs-apd-lit-tmp"
 safe_tmp.mkdir(parents=True, exist_ok=True)
+
 os.environ["DATABRICKS_CONFIG_FILE"] = str(safe_tmp / "dummy")
