ac85546bbbd489ccddfde44fd035ea2576e0be0e
codeandcodedotcom <justincaseanother@gmail.com>
2026-04-07 16:54:18 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -1,13 +1,10 @@
-from fastapi import Request
-from fastapi.responses import JSONResponse
-
-@app.exception_handler(Exception)
-async def general_exception_handler(request: Request, exc: Exception):
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
     return JSONResponse(
-        status_code=500,
-        content={"detail": str(exc)},
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
         headers={
             "Access-Control-Allow-Origin": "https://digital-dev.rolls-royce.com",
             "Access-Control-Allow-Credentials": "true",
         },
-    )
+            )
